7285f45d69772b32498b33c76d5227771c8f7fc4
Hans <marco@webkinder.ch>
2023-12-11 06:41:12 +0100

    Update pubkey function to allow ed25519 file

--- 6-fish/functions/pubkey.fish
+++ 6-fish/functions/pubkey.fish
@@ -1,3 +1,11 @@
 function pubkey
-    cat ~/.ssh/id_rsa.pub | pbcopy; and echo '=> Public key copied to pasteboard.'
+    if test -f ~/.ssh/id_rsa.pub
+        cat ~/.ssh/id_rsa.pub | pbcopy
+        echo '=> Public key copied to pasteboard.'
+    else if test -f ~/.ssh/id_ed25519.pub
+        cat ~/.ssh/id_ed25519.pub | pbcopy
+        echo '=> Ed25519 Public key copied to pasteboard.'
+    else
+        echo 'No public key file found (neither id_rsa.pub nor id_ed25519.pub).'
+    end
 end
