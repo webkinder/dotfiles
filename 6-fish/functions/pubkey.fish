db859c08bdc8c4d3e383e2cf2f72c4c5c53e9a34
Hans <marco@webkinder.ch>
2023-12-12 09:15:03 +0100

    Prefer ed25519 over id_rsa in pubkey function

--- 6-fish/functions/pubkey.fish
+++ 6-fish/functions/pubkey.fish
@@ -1,10 +1,10 @@
 function pubkey
-    if test -f ~/.ssh/id_rsa.pub
-        cat ~/.ssh/id_rsa.pub | pbcopy
-        echo '=> Public key copied to pasteboard.'
-    else if test -f ~/.ssh/id_ed25519.pub
+    if test -f ~/.ssh/id_ed25519.pub
         cat ~/.ssh/id_ed25519.pub | pbcopy
         echo '=> Ed25519 Public key copied to pasteboard.'
+    else if test -f ~/.ssh/id_rsa.pub
+        cat ~/.ssh/id_rsa.pub | pbcopy
+        echo '=> Public key copied to pasteboard.'
     else
         echo 'No public key file found (neither id_rsa.pub nor id_ed25519.pub).'
     end
